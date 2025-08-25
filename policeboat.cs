a1c95e758534b56e6b3ae98a835bad495ebe7d79
katecooper0003 <katecooper0003@gmail.com>
2025-08-25 18:23:39 -0500

    Add files via upload

---
+++ policeboat.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class policeboat : UdonSharpBehaviour
+{
+    
+    public Animator animBoat;
+
+    public override void Interact()
+    {
+        if(animBoat.GetInteger("policeBoatState") == 0)
+        {
+            animBoat.SetInteger("policeBoatState", 1);
+        }
+        else if(animBoat.GetInteger("policeBoatState") == 1)
+        {
+            animBoat.SetInteger("policeBoatState", 2);
+        }
+        else if(animBoat.GetInteger("policeBoatState") == 2)
+        {
+            animBoat.SetInteger("policeBoatState", 0);
+        }
+        
+    }
+}
